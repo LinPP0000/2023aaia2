076cf74052183640016d39540136a1b7ede4d9ab
LinPP0000 <linpp55664@gmail.com>
2024-03-20 12:00:14 +0800

    week05

---
+++ week05/week05-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        N=len(colors)
+        for i in range(N):
+            for i in range(N):
+                if colors[i] ! =color[j] and j-i > ans:
+                    ans = j-i
+        return ans
