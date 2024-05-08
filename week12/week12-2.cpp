f465d52bf6dee9dfc12a38ce638d7c5601cf2777
Linpp0000 <linp55664@gmail.com>
2024-05-08 11:44:01 +0800

    week12

--- week12/week12-2.cpp
+++ week12/week12-2.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
-using namespace std;
-int prime(int n){
-	for(int i=2;i<n;i++){
-		if(n%i==0) return 0;
-	}
-	return 1;
+///n场借计
+#include <stdio.h>
+int isPrime(int n)
+{ ///借计ぃ俱埃
+    for(int i=2; i<n; i++) {
+        if(n%i==0) return 0; ///ア毖
+    }
+    return 1; ///Θ!!! 挡Юぃ糶, Τ瑀
 }
 
-int main(){
-  int n;cin>>n;
-  cout<<"["<<prime(n)<<"]";
-  return 0;
-}
-/* よ C++  main ㄧ计 单基 よ C  main ㄧ计
-int main(void){
+int main()
+{
+    printf("叫块1计:");
     int n;
     scanf("%d", &n);
-    printf("[%d]", prime(n));
-    return 0;
+    for(int i=2; i<=n; i++) {
+        if(isPrime(i)) printf("%d ", i);
+    }
+
 }
-*/
