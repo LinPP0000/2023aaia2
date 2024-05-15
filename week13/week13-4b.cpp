80eb90b0de6f25eb82f3654c00cea26a794b09a9
Linpp0000 <linp55664@gmail.com>
2024-05-15 11:53:49 +0800

    week13

---
+++ week13/week13-4b.cpp
@@ -0,0 +1,13 @@
+///week13-4b.cpp求兩數之最大公因數
+#include<stdio.h>
+int gcd(int a, int b){
+  if(a==0)return b;
+  if(b==0)return a;///這行可不寫
+  return gcd(b, a%b);
+}///函式呼叫函式,輾轉相除法
+int main()
+{///下面是C語言的寫法,老師推薦這個
+  int a,b;
+  scanf("%d %d",&a,&b);
+  printf("%d\n",gcd(a,b));
+}
