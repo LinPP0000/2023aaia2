3a3127558a5f09cc969fb3ea931dbb000c0cf4f8
Linpp0000 <linp55664@gmail.com>
2024-06-19 11:04:09 +0800

    week18

---
+++ week18/week18-3.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int prime(int n){
+	for(int i=2;i<n;i++){
+	if(n%i==0) return 0;//bad
+	}
+	return 1;
+}
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int ans=0;
+	for(int i=a;i<=b;i++){
+		if(prime(i) ) ans++;
+	}
+	printf("%d\n",ans);
+} 
+       
+ 
+         
+ 
+  
+ 
+  
+ 
+  瘋狂程設 
+西子灣碼場 迎曦湖碼場 
+福山碼場 圓山碼場 
+福源山碼場 山仔頂碼場 
+兔子坑碼場 豆子埔碼場 
+ 
+ 
+ 
+
+
+ 
